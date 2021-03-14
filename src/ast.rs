4c8ac09f26f6cc70e73aa95f97a1f673c9f84083
Vishal Patil <vpatil3@localhost.localdomain>
2021-03-14 15:15:34 -0400

    Checkpoint

--- src/ast.rs
+++ src/ast.rs
@@ -1,12 +1,51 @@
 use crate::lexer::Token;
-use std::ptr::write_bytes;
-use std::any::Any;
-use proc_macro::Ident;
+use std::fmt;
+use std::fmt::Formatter;
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Program {
+    pub stmts: Vec<Statement>,
+}
+
+impl fmt::Display for Program {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for stmt in &self.statements {
+            write!(f, "{}", stmt)?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct BlockStatement {
+    pub stmts: Vec<Statement>,
+}
+
+impl fmt::Display for BlockStatement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{{");
+        for stmt in &self.statements {
+            write!(f, "{}", stmt)?;
+        }
+        write!(f, "}}");
+        Ok(())
+    }
+}
+
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Prefix {
     Plus,
     Bang
+}
+
+impl fmt::Display for Prefix {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Prefix::Plus => write!(f, "+"),
+            Prefix::Bang => write!(f, "!"),
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -21,6 +60,22 @@ pub enum Infix {
     Slash,
 }
 
+impl fmt::Display for Infix {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Infix::Eq => write!(f, "=="),
+            Infix::NotEq => write!(f, "!="),
+            Infix::Lt => write!(f, "<"),
+            Infix::Gt => write!(f, ">"),
+            Infix::Plus => write!(f, "+"),
+            Infix::Minus => write!(f, "-"),
+            Infix::Asterisk => write!(f, "*"),
+            Infix::Slash => write!(f, "/"),
+        }
+    }
+}
+
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum Expression {
     Identifier(String),
@@ -34,12 +89,45 @@ pub enum Expression {
     Call(Box<Expression>, Vec<Expression>),
 }
 
+impl fmt::Display for Expression {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Expression::Identifier(s) => write!(f, "{}", s),
+            Expression::IntegerLiteral(i) => write!(f, "{}", i),
+            Expression::StringLiteral(s) => write!(f, "{}", s),
+            Expression::Boolean(b) => write!(f, "{}", b),
+            Expression::Prefix(p, exp) => write!(f, "{}{}", p, exp),
+            Expression::Infix(op, left, right) =>
+                write!(f, "{} {} {}", left, op, right),
+            Expression::If(exp, true_blk, Some(false_blk)) =>
+                write!(f,"if ({}) {} else {}", exp, true_blk, false_blk),
+            Expression::If(exp, true_blk, None) =>
+                write!(f,"if ({}) {}", exp, true_blk),
+            Expression::FunctionLiteral(params, block) => write!(f, "fn({}), {}", params.join(","), block),
+            Expression::Call(exp, params) => write!(f, "{}({})", exp,
+                                                    params.iter().map(|a| a.to_string()).collect::<Vec<String>>().join(",")),
+        }
+    }
+}
+
+
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum Statement {
-    Let(Expression, Expression),
+    Let(String, Expression),
     Return(Option<Expression>),
     Expression(Expression),
-    Block(Vec<Statement>),
+}
+
+impl fmt::Display for Statement {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Statement::Let(s, exp) => write!(f, "let {} = {};", s, exp),
+            Statement::Return(None) => write!(f, "return;"),
+            Statement::Return(Some(val)) => write!(f, "return {};", val),
+            Statement::Expression(exp) => write!(f, "{}", exp),
+        }
+    }
 }
 
 #[cfg(test)]
