64a239baf7950d8ccc0e7d8cbce69506e5ed4f8b
Vishal Patil <vpatil3@cisco.com>
2021-03-17 20:26:22 -0400

    Able to evaluate infix expressions

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -17,8 +17,63 @@ pub fn eval_identifier(identifier: &Expression) -> Object
     }
 }
 
+pub fn eval_prefix_expression(prefix: &Prefix, expression: &Expression) -> Object {
+    let expr_val = eval_expression(expression);
+    match prefix {
+        Prefix::Minus => {
+            match expr_val {
+                Object::Integer(i) => Object::Integer(-1 * i),
+                _ => panic!("Invalid expression {} in prefix expression, expected integer", expr_val)
+            }
+        }
+        Prefix::Bang => {
+            match expr_val {
+                Object::Boolean(b) => Object::Boolean(!b),
+                _ => panic!("Invalid expression {} in prefix expression, expected boolean", expr_val)
+            }
+        }
+    }
+}
+
+pub fn eval_infix_expression(infix: &Infix, left: &Expression, right: &Expression) -> Object {
+    let left_obj = eval_expression(left);
+    let right_obj = eval_expression(right);
+
+    let left_val = match left_obj {
+        Object::Integer(i) => i,
+        _ => panic!("Invalid value in expression {}, expected int", left_obj),
+    };
+    let right_val = match right_obj {
+        Object::Integer(i) => i,
+        _ => panic!("Invalid value in expression {}, expected int", right_obj),
+    };
+
+    match infix {
+        Infix::Plus => Object::Integer(left_val + right_val),
+        Infix::Minus => Object::Integer(left_val - right_val),
+        Infix::Asterisk => Object::Integer(left_val * right_val),
+        Infix::Slash => Object::Integer(left_val / right_val),
+        Infix::NotEq => Object::Boolean(left_val != right_val),
+        Infix::Eq => Object::Boolean(left_val == right_val),
+        Infix::Gt => Object::Boolean(left_val > right_val),
+        Infix::Lt => Object::Boolean(left_val < right_val),
+        _ => panic!("Invalid op {}", infix)
+    }
+}
+
 pub fn eval_expression(expr: &Expression) -> Object {
-    Object::Nil
+    match expr {
+        Expression::IntegerLiteral(i) => Object::Integer(*i),
+        Expression::Identifier(s) => Object::Identifier(s.to_string()),
+        Expression::Boolean(b) => Object::Boolean(*b),
+        Expression::Prefix(prefix, expr) =>
+            eval_prefix_expression(prefix, expr),
+        Expression::Infix(infix, left, right) =>
+            eval_infix_expression(infix, left, right),
+        Expression::FunctionLiteral(params, block) =>
+            Object::FunctionLiteral(params.clone(), *block.clone()),
+        _ => Object::Nil
+    }
 }
 
 pub fn eval_let_statement(identifier: String, expr: &Expression) -> Object {
@@ -30,16 +85,94 @@ pub fn eval_return_statement(expr: &Expression) -> Object {
     eval_expression(expr)
 }
 
-pub fn eval_program(program: Program) -> Object
+pub fn eval_program(program: &Program) -> Object
 {
     let mut val = Object::Nil;
-    for stmt in program.stmts {
+    for stmt in &program.stmts {
         val = match stmt {
-            Statement::Let(id, expr) => eval_let_statement(id, &*expr),
-            Statement::Return(Some(expr)) => {return eval_return_statement(&*expr)},
-            Statement::Return(None) => {return Object::Nil},
+            Statement::Let(id, expr) => eval_let_statement(id.to_string(), &*expr),
+            Statement::Return(Some(expr)) => { return eval_return_statement(&*expr); }
+            Statement::Return(None) => { return Object::Nil; }
             Statement::Expression(expr) => eval_expression(&*expr),
         };
     }
     return val;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::lexer::{Lexer, Token};
+    use crate::parser::Parser;
+    use std::any::Any;
+    use crate::object::Object;
+    use crate::evaluator::*;
+    use std::process::id;
+
+    fn test_eval_program(input: &str) -> Object {
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+        let program = parser.parse_program().unwrap();
+        eval_program(program.as_ref())
+    }
+
+    struct TestCase<'a> {
+        test_str: &'a str,
+        val: Object,
+    }
+
+    fn check_test_cases(test_cases: Vec<TestCase>) {
+        for test_case in test_cases {
+            assert_eq!(test_eval_program(test_case.test_str), test_case.val);
+        }
+    }
+
+    #[test]
+    fn test_eval_integer() {
+        let test_cases = vec![TestCase { test_str: "10", val: Object::Integer(10) }];
+        check_test_cases(test_cases);
+    }
+
+    #[test]
+    fn test_eval_boolean() {
+        let test_cases = vec![
+            TestCase { test_str: "true", val: Object::Boolean(true) },
+            TestCase { test_str: "false", val: Object::Boolean(false) },
+        ];
+        check_test_cases(test_cases);
+    }
+
+    #[test]
+    fn test_eval_return() {
+        let test_cases = vec![
+            TestCase { test_str: "return 0;", val: Object::Integer(0) }
+        ];
+
+        check_test_cases(test_cases);
+    }
+
+    #[test]
+    fn test_eval_prefix() {
+        let test_cases = vec![
+            TestCase { test_str: "!true", val: Object::Boolean(false) },
+            TestCase { test_str: "-1", val: Object::Integer(-1) }
+        ];
+
+        check_test_cases(test_cases);
+    }
+
+    #[test]
+    fn test_eval_infix() {
+        let test_cases = vec![
+            TestCase { test_str: "10 > 20", val: Object::Boolean(false) },
+            TestCase { test_str: "-1 + 0", val: Object::Integer(-1) },
+            TestCase { test_str: "5 > 3", val: Object::Boolean(true)},
+            TestCase {test_str: "4 < 2", val: Object::Boolean(false)},
+            TestCase {test_str: "5 == 3", val: Object::Boolean(false)},
+            TestCase {test_str: "4 != 2", val: Object::Boolean(true)},
+            TestCase {test_str: "(2*2 + 1) == 5", val: Object::Boolean(true)},
+            TestCase {test_str: "(2 + 3)*2 == 10", val: Object::Boolean(true)},
+        ];
+
+        check_test_cases(test_cases);
+    }
 }
